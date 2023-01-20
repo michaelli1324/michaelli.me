fc2594ed1bbf620aa937234630489043105290af
Michael Li <michael.li1324@gmail.com>
2023-01-19 19:21:21 -0800

    [Zod] Use zod for env variable validation

--- environment.d.ts
+++ environment.d.ts
@@ -1,13 +1,20 @@
+import { z } from 'zod';
+
+const envVariables = z.object({
+  SPOTIFY_CLIENT_ID: z.string(),
+  SPOTIFY_CLIENT_SECRET: z.string(),
+  SPOTIFY_REFRESH_TOKEN: z.string(),
+});
+
+envVariables.parse(process.env);
+
 declare global {
   namespace NodeJS {
-    interface ProcessEnv {
-      SPOTIFY_CLIENT_ID: string;
-      SPOTIFY_CLIENT_SECRET: string;
-      SPOTIFY_REFRESH_TOKEN: string;
-    }
+    // eslint-disable-next-line @typescript-eslint/no-empty-interface
+    interface ProcessEnv extends z.infer<typeof envVariables> {}
   }
 }
 
 // If this file has no import/export statements (i.e. is a script)
 // convert it into a module by adding an empty export statement.
-export {}
+export {};
